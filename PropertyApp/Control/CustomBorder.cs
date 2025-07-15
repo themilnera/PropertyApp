9057ea8b0f852f7519be7e874631448c70fe5d5c
themilnera <amilner32@gmail.com>
2025-07-15 14:13:09 -0600

    Added logic for random custom border design for the images on the main landing page, added an icon to the details page.

---
+++ PropertyApp/Control/CustomBorder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyApp.Control
+{
+    public class CustomBorder : Border
+    {
+        public CustomBorder()
+        {
+            AddCornerRadius();
+        }
+
+        private List<int> CornerValues = new List<int> { 10, 40, 70, 100 };
+        private void AddCornerRadius()
+        {
+            var index = new Random().Next(4);
+            StrokeShape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(CornerValues[index])
+            };
+        }
+    }
+}
