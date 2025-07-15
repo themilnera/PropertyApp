0621e92df28505eedd0c32ab489653f4453ad5ff
themilnera <amilner32@gmail.com>
2025-07-15 14:33:11 -0600

    Added animations for the details page.

--- PropertyApp/Control/CustomBorder.cs
+++ PropertyApp/Control/CustomBorder.cs
@@ -11,7 +11,15 @@ namespace PropertyApp.Control
     {
         public CustomBorder()
         {
+            TranslationX = new Random().Next(-500, 500);
+            Rotation = Math.Max(TranslationX, 100);
             AddCornerRadius();
+
+            Loaded += (s, e) =>
+            {
+                this.TranslateTo(0, 0, 1500, Easing.SinInOut);
+                this.RotateTo(0, 1500, Easing.SinInOut);
+            };
         }
 
         private List<int> CornerValues = new List<int> { 10, 40, 70, 100 };
--- PropertyApp/View/DetailsPage.xaml.cs
+++ PropertyApp/View/DetailsPage.xaml.cs
@@ -13,5 +13,52 @@ public partial class DetailsPage : ContentPage
 		viewModel.MoreItems = selectedProperty.Images.Count - 2;
 		 
 		this.BindingContext = viewModel;
+
+		SetViewPositions();
+
+		Loaded += (s, e) =>
+		{
+			FadeAndScale(detailsBtn);
+			RotateView(detailsBtn);
+			FadeAndTranslate(imagesView);
+
+			Task.Delay(500);
+
+			FadeAndTranslate(popView, 1000, 1000);
+			FadeAndTranslate(addressView, 1000, 1000);
+			FadeAndScale(buyBtn, 1000, 1000);
+		};
 	}
+	private void SetViewPositions()
+	{
+		detailsBtn.Opacity = 0;
+		detailsBtn.Scale = 0.2;
+		detailsBtn.Rotation = 300;
+
+		imagesView.TranslationX = 300;
+		imagesView.Opacity = 0;
+
+		addressView.TranslationX = addressView.TranslationY = -30;
+		addressView.Opacity = 0;
+
+		buyBtn.Opacity = 0;
+		buyBtn.Scale = 0.2;
+
+		popView.TranslationY = 300;
+		popView.Opacity = 0.6;
+	}
+
+	private void FadeAndTranslate(VisualElement view, uint fadeLength = 1000, uint translateLength = 1500)
+	{
+		view.FadeTo(1, fadeLength, Easing.SinInOut);
+		view.TranslateTo(0, 0, translateLength, Easing.SinInOut);
+	}
+
+	private void FadeAndScale(VisualElement view, uint fadeLength = 1000, uint scaleLength = 1500)
+	{
+		view.FadeTo(1, fadeLength, Easing.SinInOut);
+		view.ScaleTo(1, scaleLength, Easing.SinInOut);
+	}
+
+	private void RotateView(VisualElement view) => view.RotateTo(0, 1500, Easing.SinInOut);
 }
